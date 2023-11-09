45cb614fd903b019d443331f47d693c8e78010ac
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2023-11-09 13:08:27 +0630

    add design

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,23 +1,26 @@
 <template lang="pug">
-.max-w-6xl.mx-auto.bg-white.shadow-lg.rounded-lg.overflow-hidden.mt-16
-  .px-4.py-2.border-b-2
-    h1.text-gray-800.font-bold.text-2xl.uppercase {{$t("message")}}
-    span.text-gray-500 (vue-pug,vuex.3,vue.2,vuex-helper,vue-composition-api)
+.max-w-6xl.mx-auto.bg-white.shadow-lg.rounded-lg.overflow-hidden.mt-16.border.border-teal-300
+  .px-4.py-2.border-b.border-teal-200
+    .flex.items-center.justify-between
+      div
+        h1.font-bold.text-2xl.uppercase {{$t("todoList")}}
+        span.text-gray-400 (vue-pug,vuex.3,vue.2,vuex-helper,vue-composition-api,vue-i18n)
       LocaleSwitcher
-  form.w-full.max-w-sm.mx-auto.px-4.py-2
+  div.w-full.max-w-sm.mx-auto.px-4.py-2
     .flex.items-center.border-b-2.border-teal-500.py-2
-      input(class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1.px-2.leading-tight focus:outline-none" type="text", placeholder="Add a task" v-model="newTodo.title")
-      button(class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button" @click="addTodo(newTodo)").
+      input(class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1.px-2.leading-tight focus:outline-none" type="text", :placeholder="$t('placeholder')" v-model="newTodo.title")
+      button(class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button" @click="addTodoItem(newTodo)").
         
-        Add
+        {{ $t('add') }}
         
-  ul.divide-y.divide-gray-200.px-4
-    li.py-4(v-for="todo in  myTodos" :key="todo.id")
-      .flex.items-center
+  ul.px-4.flex.flex-wrap.mt-4
+    li.py-4(v-for="todo in  myTodos" :key="todo.id" class="w-1/3 p-3")
+      .p-3.rounded.bg-white.shadow
+        .flex.flex-row-reverse
+          button(@click="deleteTodo(todo.id)")
+            delete-icon
         label.ml-3.block.text-gray-900(for="todo1")
           span.text-lg.font-medium {{ todo.title }}
-        button(@click="deleteTodo(todo.id)")
-          delete-icon
 </template>
 
 <script>
@@ -48,6 +51,10 @@ export default {
       "deleteTodo",
     ]);
     const { myTodos } = useGetters(["myTodos"]);
+    const addTodoItem = (newTodo) => {
+      addTodo(newTodo);
+      newTodo.title = "";
+    };
 
     onMounted(() => {
       getTodos();
@@ -58,6 +65,7 @@ export default {
       myTodos,
       newTodo,
       addTodo,
+      addTodoItem,
       deleteTodo,
     };
   },
--- src/components/language/LocaleSwitcher.vue
+++ src/components/language/LocaleSwitcher.vue
@@ -1,10 +1,8 @@
-<template>
-  <div class="locale-switcher">
-    <select v-model="$i18n.locale">
-      <option value="en">English</option>
-      <option value="mr">myanmar</option>
-    </select>
-  </div>
+<template lang="pug">
+.locale-switcher
+  select(class="block w-full py-1 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none shadow focus:ring-blue-500 focus:border-blue-500 px-2" v-model="$i18n.locale")
+    option(value="en") English
+    option(value="mr") myanmar
 </template>
 <script>
 export default {
